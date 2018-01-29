75fb3e3890e71fbc7e5035c913a5a9fdf927bdc8
Jakob Hohlfeld <jhohlfeld@polyban.io>
2018-01-29 22:02:52 +0100

    Change colors, fonts and logo atoms and theme.

---
+++ src/themes/colors.js
@@ -0,0 +1,8 @@
+export default {
+  darkblue: "hsl(195, 87%, 47%)",
+  lightblue: "hsl(193, 74%, 62%)",
+  darkgrey: "hsl(0, 0%, 70%)",
+  lightgrey: "hsl(0, 0%, 84%)",
+  black: "hsl(0, 0%, 0%)",
+  white: "hsl(0, 0%, 100%)"
+};
